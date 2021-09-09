ebbd5d88d2757c7911c99f186f8c61f3a36a4bea
Sabaat172 <sabaat172@gmail.com>
2021-09-09 11:09:19 +0530

    Update test.py

--- machinetranslation/tests/test.py
+++ machinetranslation/tests/test.py
@@ -1,1 +1,29 @@
+import unittest
+from translator import english_to_french, french_to_english
 
+
+class TestFr(unittest.TestCase):
+    
+
+    def test_hello(self):
+        self.assertEqual(english_to_french('Hello'), 'Bonjour')
+
+    def test_none(self):
+        firstValue = None
+        msg=None
+        self.assertEqual(english_to_french('Text'), 'Texte')
+
+
+class TestEn(unittest.TestCase):
+    
+
+    def test_hello(self):
+        self.assertEqual(french_to_english('Bonjour'), 'Hello')
+
+    def test_none(self):
+        firstValue = None
+        msg=None
+        self.assertEqual(french_to_english('Texte'), 'Text')
+
+
+unittest.main()
